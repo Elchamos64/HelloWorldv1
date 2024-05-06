807b7034b84dc1bbe158845e5950876d19080cbe
Oscar Ramos <131794552+Elchamos64@users.noreply.github.com>
2024-05-06 12:25:05 -0600

    added rectangle function

--- shapes.py
+++ shapes.py
@@ -1,6 +1,12 @@
 #  Python
 
+def calculate_rectangle_are(width, height):
+    area = width * height
+    return area
+
 def main():
     print("This is our shapes program")
+    rectangle_area = calculate_rectangle_are(10, 20)
+    print(f'the area of my rectangle is : {rectangle_area}')
 
 main()
