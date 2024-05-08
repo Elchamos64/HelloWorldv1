30e44d125d2c8176036d719d71dc54fbfb664fbf
Oscar Ramos <131794552+Elchamos64@users.noreply.github.com>
2024-05-08 11:57:11 -0600

    added calculate_triangle_area function

--- shapes.py
+++ shapes.py
@@ -4,9 +4,17 @@ def calculate_rectangle_are(width, height):
     area = width * height
     return area
 
+def calculate_triangle_perimeter(height, base):
+    area = 1/2 * base * height
+    return area
+
 def main():
     print("This is our shapes program")
     rectangle_area = calculate_rectangle_are(10, 20)
     print(f'the area of my rectangle is : {rectangle_area}')
+
+
+    triangle_area = calculate_triangle_perimeter(20, 40)
+    print(f'the area of my triangle is : {triangle_area}')
 
 main()
